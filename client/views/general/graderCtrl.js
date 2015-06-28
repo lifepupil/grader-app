fc5f30d84d90bac17895b53aaaa5a33e3c52a05d
Christian Richard <lifepupil@gmail.com>
2015-06-28 14:58:36 -0700

    list students

--- client/views/general/graderCtrl.js
+++ client/views/general/graderCtrl.js
@@ -4,6 +4,8 @@ angular.module('grader-app')
 .controller('gradingCtrl', function($scope){
   console.log('inside the graderCtrl');
 
+  $scope.students = ['john', 'mary', 'fred', 'jose'];
+
   $scope.addStudent = function(student){
     console.log('student', student);
     Student.add(student);
